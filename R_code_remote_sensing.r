5172b3fb2a6ecef46d7ecd18db7e2f4f832e5371
Sara Sena <92572689+saracurly97@users.noreply.github.com>
2021-10-25 10:31:11 +0200

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,11 @@
+# R code for ecosystem monitoring by remote sensing 
+# first of all, we need to install additional packages
+# raster pckage to manage image data 
+# https://cran.r-project.org/web/packages/raster/index.html
+
+install.packages("raster")
+# C:\Users\saras\AppData\Local\Temp\RtmpoZejaJ\downloaded_packages
+
+library(raster)
+
+install.packages("terra")
