5589e194eb9ac9bf7535aff1b5657988920ce720
Sara Sena <92572689+saracurly97@users.noreply.github.com>
2021-11-08 10:57:23 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -9,12 +9,12 @@ install.packages("raster")
 library(raster)
 
 # windows
-#I have to write this
+#I have to write this 
 setwd("C:/Users/saras/Desktop/lab/")  
 
 
 # we are going to import satellite data 
-# objects cannot be numbers
+# objects cannot be numbers #brick function to import images 
 l2011 <- brick("p224r63_2011.grd")
 
 l2011
@@ -62,5 +62,47 @@ par(mfrow=c(2, 1))
 plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
 
+#--- day 3
+
+plot(l2011$B1_sre)
+
+#plot the blue band using a blue colorRampPalette
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+#multiframe
+par(mfrow=c(1, 2)) 
+
+#plot the blue and the green bedides, with different colorRampPalette
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+par(mfrow=c(2, 1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# exercise: plot the first four bands with two  rows and two colums 
+par(mfrow=c(2, 2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c("dark red","red","pink"))(100)
+plot(l2011$B3_sre, col=clr)
+
+clnir <- colorRampPalette(c("red","orange","yellow"))(100)
+plot(l2011$B4_sre, col= clnir)
+
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")  # false colours
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") 
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") 
 
 
