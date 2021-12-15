cdb8937fb747cd42d318cc292a1a21b0b7a79feb
Sara Sena <92572689+saracurly97@users.noreply.github.com>
2021-12-15 17:57:15 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,5 +1,13 @@
-#R code for uploading and visualizing copernicus a
+#R code for uploading and visualizing copernicus data in R
 
+install.packages("ncdf4")
 library(ncdf4)
+
 # Set the working directory
 setwd("C:/Users/saras/Desktop/lab/copernicus")
+
+snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
+
+snow20211214
+
+plot(snow20211214)
